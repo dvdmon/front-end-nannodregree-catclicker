6d8d03c6a813377f138f958160aab4a70f40da69
dvdmon <levi@dvdmon.com>
2016-10-01 16:40:48 -0400

    Added second cat to click and captions
    I also redid some of the script to use only jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,25 @@
 $(function() {
-	var elem = document.getElementById('catImage');
-	var elemCount = elem.innerText;
-	elem.addEventListener('click', function(){
-	  elemCount++;
-	  elem.innerText = elemCount;
-	}, false);
+	var $cat1Image = $('#catImage1');
+	var $cat2Image = $('#catImage2');
+
+	$cat1Image.css("background-image","url('http://lorempixel.com/400/300/cats/')");
+	$cat2Image.css("background-image","url('http://lorempixel.com/300/300/cats/')");
+	$('#catCaption1').text("Harry");
+	$('#catCaption2').text("Betty");
+    //console.log( "ready!" );
+
+
+	var cat1Count = $cat1Image.text();
+	var cat2Count = $cat2Image.text();
+
+	$cat1Image.click(function(){
+	  cat1Count++;
+	  $cat1Image.text(cat1Count);
+	});
+
+	$cat2Image.click(function(){
+	  cat2Count++;
+	  $cat2Image.text(cat2Count);
+	});
+
 });
