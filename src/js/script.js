3aa45990c2a9bafdcf261f32bf8de539818c4356
dvdmon <levi@dvdmon.com>
2016-10-02 12:42:48 -0400

    Fix: overhauled the code to create and remove image divs
    The previous problem was undoubtedly due to multiple image divs
    occupying the same space, and each having their own click event handlers
    which were created whenever they were displayed, and thus creating multiple
    handlers for the same click.  Even setting display to none did not solve
    this issue, and so in order to get it working I had to actually create and
    remove the image div nodes each time a new list item was selected, so that
    the event handlers wouldn't combine.  I was able to store the counts in a
    separate array that was keyed based on the name of the cat.

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,65 @@
 $(function() {
 
-	//set up array variable to hold cat elements
-	var $cats = $("#catList").children("li");
-	var catCounts = [0,0,0,0,0,0,0]
-	var $catImage = $('#catImage');
-	for (var i = 0; i < $cats.length; i++) {
-		var cat = $cats[i];
+	//set up array variable to hold cat list item elements
+	var $catListItems = $("#catList").children("li");
 
-		cat.addEventListener('click', (function(currentCat) {
+	// create array of objects to hold cat item information
+	var cats = [];
+	for (i = 0; i < $catListItems.length; i++)
+	{
+		cats[i] = {
+			elem: $catListItems[i],
+			count: 0,
+			name: $catListItems[i].innerText
+			}
+	}
+
+
+	var $catFrame = $("#catImageFrame");
+
+	// loop through cat list elements to create event listeners
+	for (var i = 0; i < $catListItems.length; i++) {
+		var catListItem = $catListItems[i];
+
+
+
+		// create event listener with a closure that does something when you click on a cat list item
+		catListItem.addEventListener('click', (function(currentCat) {
         	return function() {
-        		//alert(cat.innerText)
-        		$catImage.css("background-image","url('../src/images/" + currentCat.innerText + ".jpg')");
-				$('#catCaption').text(currentCat.innerText);
-				$catImage.text("0");
-				//catCounts[$("#catList").find(currentCat).index()] = 0;
-				$catImage.click(function(){
-	  				catCounts[$("#catList").find(currentCat).index()] +=1;
-	  				console.log(catCounts);
 
-	  				//console.log($("#catList").find(currentCat).index());
-	  				$catImage.text(catCounts[$("#catList").find(currentCat).index()]);
+        		catName = currentCat.textContent;
+        		cat = cats.filter(function (obj) {
+        				return (obj.name === catName);
+        			});
+
+
+        		// first make sure any previously displayed cat div is no longer present
+        		$("#catImageFrame").children().remove();
+
+        		//insert the divs the cat, including their images and captions
+        		catDivId = "#innerCatFrame-" + catName;
+				$catFrame.append('<div id="innerCatFrame-' + catName + '">' +  '</div><div id="catCaption-' + catName + '">' + catName + '</div>');
+				$(catDivId).css({"background-image":"url('../src/images/" + catName + ".jpg')","display":"block"});
+				$(catDivId).text(cat[0].count);
+				$catFrame.children().css("display","block");
+
+        		//set up another event handler within the current cat's image div
+        		$(catDivId).click(function(){
+
+        			//find the array element in the cats array that matches the current cat
+        			 cat = cats.filter(function (obj) {
+        				return ("innerCatFrame-" + obj.name === $(catDivId).attr("id"));
+        			});
+
+        			// increment the count property of the cat array element
+        			cat[0].count += 1;
+        			// assign the new count to the inner text of the cat image
+					$(catDivId).text(cat[0].count);
 				});
-	   		 };
-		})(cat));
+   		 };
+		})(catListItem));
+
+
 
 	};
 
